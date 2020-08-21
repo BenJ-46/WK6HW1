a365809cd0261c4d723a987815384a8a08602d3a
bennaayy <jimenezbenjamin338@gmail.com>
2020-08-20 21:55:54 -0700

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
+const {join} = require('path')
+
 
 router.use('/api', require('./apiRoutes.js'))
-router.use('/', require('.htmlroutes.js'))
+router.use('/', require('./htmlroutes.js'))
+
 module.exports = router
