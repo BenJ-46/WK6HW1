8d933f1a6dc84f41e40464a5fd5cfeede82cb6fb
bennaayy <jimenezbenjamin338@gmail.com>
2020-08-20 19:13:20 -0700

    adding routes path on server file

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(require('./routes'))
 
 
-app.listen(3000)
-
+app.listen(process.env.PORT || 3000)
 //need a folder titled "public" and a HTML file titled "index.html"
